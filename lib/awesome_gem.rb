b0e70eeaecc710fccc065c8aafa32d15fb3f5579
Mateus Couto <mateus.couto@airtasker.com>
2020-09-09 13:41:46 +1000

    test

---
+++ lib/awesome_gem.rb
@@ -0,0 +1,7 @@
+require "awesome_gem/version"
+require "awesome_gem/test"
+
+module AwesomeGem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/awesome_gem/test.rb
@@ -0,0 +1,7 @@
+module AwesomeGem
+  class Test
+    def self.run
+      puts "Running Awesome Gem!!!"
+    end
+  end
+end
